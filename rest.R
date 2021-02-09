4f4b463f57ba503789b9de6028280c60859412ef
Christian B. Knudsen <cbk@kb.dk>
2021-02-09 09:05:37 +0100

    Hm.

---
+++ rest.R
@@ -0,0 +1,6 @@
+test
+
+
+system("where git")
+system("echo $PATH")
+/mingw64/bin:/usr/bin:/c/Users/cbk/bin:/c/usr/bin:/c/Program Files/R/R-4.0.2/bin/x64:/c/Program Files (x86)/Python37-32/Scripts:/c/Program Files (x86)/Python37-32:/c/WINDOWS/system32:/c/WINDOWS:/c/WINDOWS/System32/Wbem:/c/WINDOWS/System32/WindowsPowerShell/v1.0:/c/ProgramData/Oracle/Java/javapath:/c/Strawberry/c/bin:/c/Strawberry/perl/site/bin:/c/Strawberry/perl/bin:/c/Program Files/PuTTY:/c/WINDOWS/System32/OpenSSH:/c/Program Files/Microsoft SQL Server/120/Tools/Binn:/c/Program Files/ojdkbuild/java-1.8.0-openjdk-1.8.0.282-1/bin:/c/Program Files/ojdkbuild/java-1.8.0-openjdk-1.8.0.282-1/jre/bin:/cmd:/c/Users/cbk/AppData/Local/Microsoft/WindowsApps:/c/Users/cbk/AppData/Roaming/Python/Python37/Scripts/WindowsApps:/bin:/c/Program Files/RStudio/bin/msys-ssh-1000-18
